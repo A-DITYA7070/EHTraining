ac9e860e4c8af769c0db8fd89be76e8c42ca47ae
Aditya raj <aditya758.hitcsecs2020@gmail.com>
2024-03-06 01:00:37 +0530

    added pocs

---
+++ POCS/csrfLabs/que2/server.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const path=require("path");
+
+const app = express();
+
+app.get("/hack",(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+
+app.listen(4500,()=>{
+    console.log(`server is running `)
+})
